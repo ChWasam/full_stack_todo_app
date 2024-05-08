6d660c63f60f9356969d1857471b62a5b539b339
ChWasam <wasam.developer@gmail.com>
2024-05-08 13:38:51 +0500

    update

--- backend_todo/taskTrack_todo/README.md
+++ backend_todo/taskTrack_todo/README.md
@@ -9,7 +9,7 @@ Steps
 2. pydantic => Data validation and data parsing
 3. sql alchemy => data base me tables create kara ga
 4. sql model => pydantic + sqlalchemy
-5. we will create model of 2 types 1. Data Model (data ko validate karna ka lia ) 2. Table model (data base me jo table create karna ha aus ka lia )
+5. we need to  create model of 2 types 1. Data Model (data ko validate karna ka lia ) 2. Table model (data base me jo table create karna ha aus ka lia )
 
 6. Jab alchemy use hota tha to aus case me 1. Data Model alag banta tha 2. Table model alag banta tha
 
@@ -18,11 +18,11 @@ Need to make only one model
 
 8. 2nd step Connecting our app with database - .env file - copy connection string and paste it in .env file
 
-9. DATABASE_URL ko secrete rakhna ka lia settings.py ke file bnata han
+9. DATABASE_URL ko secret rakhna ka lia settings.py ke file bnata han
 
 10. now import DATABASE_URL from setting jo hama engine create karna me madad da ga
 
-11. as we nned to setup our connection with database - fastapi -> ka andar sqlmodel-> ka andar engine - yeh engine hmara connection karwai ga db (postgres) ka sath - wo engine ai ga sqlmodel sa
+11. as we need to setup our connection with database - fastapi -> ka andar sqlmodel-> ka andar engine - yeh engine hmara connection karwai ga db (postgres) ka sath - wo engine ai ga sqlmodel sa
     -from sqlmodel import create_engine
 
 12 Now create engine 
--- backend_todo/taskTrack_todo/app/main.py
+++ backend_todo/taskTrack_todo/app/main.py
@@ -4,19 +4,19 @@ from app import settings
 
 #  Lets make a data model yeh table create nahi kara ga 
 #  table = True pass karna para ga agar ham chahta han kah yeh table bhi create kara 
-#  Field() : Yeh e.g id ka name sa table me field create kara ha 
-#  id is primary ke ku kah filhal ak hi table ha 
+#  Field() : Yeh e.g id ka name sa table me field create karta ha 
+#  id is primary key ku kah filhal ak hi table ha 
 #  primary key should be unique 
-#  Foreign key aus waqt chahia ho ge jab han banai ga user ka model 
+#  Foreign key aus waqt chahia ho ge jab ham banai ga user ka model 
 
-#  AB id ko bnana ha primary key 
-#  aur wo unique bbhi honi chahia
+#  Ab id ko bnana ha primary key 
+#  aur wo unique bhi honi chahia
 #  Agar user  sa  id lai to wo to asi id da sakta ha jo pehla nhi kisi na de ho 
 #  Ham yeh chahta ham db yeh khud create kara  
 # is ka lia ham   pehla id : int|None dana para ga . None ka matlab absence of data 
 #  jab todo ko get kar kah  la kar ai ga aus case me id ho ga 
 # Ku kah aus case me database na assign kia ho ga  
-#  jab create kara ga to aus waqt none ho ga ku kah ham id to nahi da raha han
+#  jab todo ko create kara ga to aus waqt none ho ga ku kah ham id to nahi da raha han
 
 
 class Todo (SQLModel, table = True):
@@ -24,16 +24,39 @@ class Todo (SQLModel, table = True):
     content : str = Field(index=True, min_length=3, max_length=54)
     # Index : Is column ko data base me index kar da ge 
     # Faida yeh hota ha kha jab ham search karain ga apna task ko  
-    #  to sara database ko sara table scan nahi karna para ga 
+    #  to database ko sara table scan nahi karna para ga 
 
-    #  data validation ka lia bhi use hota ha is lia min = 3
+    #  data validation ka lia bhi use hota ha is lia min_length=3
     is_completed = bool = Field(default=False)
     #  Matlab jab bhi create karain ga task to default value False kar dya ha 
 
- 
+# *******************************************************************
+#  Engine is one for whole application 
+
 connection_string:str = str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
-#  ku kah me is postgrs ko psycopg ka sath use kar raha ho 
-engine = create_engine(connection_string)
+#  ku kah me is postgrs ko psycopg ka sath use kar raha ho is lia replace kia ha 
+#  kah mera communication is sa ho ge postgresql+psycopg 
+
+#  Ab ham chahta han jo bhi hmari communication ho database ka sath wo encrypt ho kah jai 
+#  so we will add secure socket layer(ssl)
+engine = create_engine(connection_string, connect_args={"sslmode":"require "},pool_recycle=300)
+
+#   Hamara jo engine ha wo connection pool banai ga 
+#  wo ak connection hmara database ka sath nahi kara ga 
+#   by default sqlslchemy me  jab app start ho ge to 5 connection establish karta ha 
+#  lakin ham change kar sakta han pool_size=10 is tarha sa
+#  matlab wo connection ka pool bna raha ha 
+
+#  naya connection jab database ka sath bana ga to aus ka aupar time lagta ha 
+#  wo time hamara waste ho raha ha 
+#  jasa waiter ai ga order lai ga kitchen me jai ga aur wapis order lana chala jai ga 
+#  yani connection standby ho ga 
+
+#  pool_recycle matlab kah 300 sec yani 5 min bad connection recycle ho jai 
+
+# *******************************************************************
+
+
 
 
 
