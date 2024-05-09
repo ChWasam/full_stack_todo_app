d908ddc345967a88e7b2fd487d6231d67811ac6c
ChWasam <wasam.developer@gmail.com>
2024-05-09 10:25:59 +0500

    Backend of todo app completed. Yet to write the tests

--- backend_todo/taskTrack_todo/README.md
+++ backend_todo/taskTrack_todo/README.md
@@ -9,7 +9,7 @@ Steps
 2. pydantic => Data validation and data parsing
 3. sql alchemy => data base me tables create kara ga
 4. sql model => pydantic + sqlalchemy
-5. we need to  create model of 2 types 1. Data Model (data ko validate karna ka lia ) 2. Table model (data base me jo table create karna ha aus ka lia )
+5. we need to create model of 2 types 1. Data Model (data ko validate karna ka lia ) 2. Table model (data base me jo table create karna ha aus ka lia )
 
 6. Jab alchemy use hota tha to aus case me 1. Data Model alag banta tha 2. Table model alag banta tha
 
@@ -25,9 +25,36 @@ Need to make only one model
 11. as we need to setup our connection with database - fastapi -> ka andar sqlmodel-> ka andar engine - yeh engine hmara connection karwai ga db (postgres) ka sath - wo engine ai ga sqlmodel sa
     -from sqlmodel import create_engine
 
-12 Now create engine 
+12 Now create engine
 
-13. ab hama koi asi chezz chahia jo orm ke commands ko sql me convert kar kah database ko da 
+13. ab hama koi asi chezz chahia jo orm ke commands ko sql me convert kar kah database ko da
 
-14. python hama postgresql databases ka lia ak driver provide karta ha 
-        - poetry add "psycopg[binary]"
+14. python hama postgresql databases ka lia ak driver provide karta ha - poetry add "psycopg[binary]"
+
+15 ab jo model bnaya tha table ka isa create bhi karna ha - Jo Todo class ka data/Fields ho ga wo SQLModel ka metadata attribute ka sath automatically register ho gya (lakin aus ka lia table=True hona chahia)
+
+16 session (Ak app me engine ak ho ga session different create ho ga)
+
+- har function ka lia ak alag sa session create ho ga
+  -e.g login har user ka lia ak alag sa session create hota ha - Jab user logout hta ha to wo session close hota ha - jab ak todo bana ge to wo session ka through ho ga - pehla session create karain ga phir session ko close bhi karna para ga - ak engine me mukhtalif session ho ga jo aus engine ka use kar kah jo bhi task ho ga wo perform karwai ga
+
+17 Create todos in database
+-session.add()
+-session.commit()
+-session.refresh()
+-session.close()
+
+18. Fastapi
+
+19. ab har method(get, del, post,put) ka lia session ka sara kam karna para ga - generator function - dependency injection
+
+20. Jab hamari app start hoti ha to wo khuch cheezo pa depend kar rahi hoti ha
+
+- tables pa depend karti ha
+- Jab user endpoint pa ata ha aur wo todo pass karta ha to todo database me create hona sa pehla khuch kam hona chahia 1. connections ready hona chahia 2. tables create  hona chahia
+- Hama is ka lia ak context bnana para ga 
+
+21. ak command ha select yeh ham sqlmodel sa import karain ga 
+- Yeh select kara ga table ko 
+
+
--- backend_todo/taskTrack_todo/app/main.py
+++ backend_todo/taskTrack_todo/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
-from sqlmodel import SQLModel,Field, create_engine
+from fastapi import FastAPI, Depends,HTTPException
+from sqlmodel import SQLModel,Field, create_engine, Session, select
 from app import settings
+from typing import Annotated
+from contextlib import asynccontextmanager
+#  Custom type create karna ka lia Annotated ka use karta han 
 
 #  Lets make a data model yeh table create nahi kara ga 
 #  table = True pass karna para ga agar ham chahta han kah yeh table bhi create kara 
@@ -19,27 +22,28 @@ from app import settings
 #  jab todo ko create kara ga to aus waqt none ho ga ku kah ham id to nahi da raha han
 
 
-class Todo (SQLModel, table = True):
+class Todo (SQLModel, table=True):
     id : int|None = Field(default=None, primary_key=True)
-    content : str = Field(index=True, min_length=3, max_length=54)
+    # Yahan id optional ha 
+    content: str = Field(index=True, min_length=3, max_length=54)
     # Index : Is column ko data base me index kar da ge 
     # Faida yeh hota ha kha jab ham search karain ga apna task ko  
     #  to database ko sara table scan nahi karna para ga 
 
     #  data validation ka lia bhi use hota ha is lia min_length=3
-    is_completed = bool = Field(default=False)
+    is_completed: bool = Field(default=False)
     #  Matlab jab bhi create karain ga task to default value False kar dya ha 
 
 # *******************************************************************
 #  Engine is one for whole application 
 
-connection_string:str = str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
+connection_string: str =str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
 #  ku kah me is postgrs ko psycopg ka sath use kar raha ho is lia replace kia ha 
 #  kah mera communication is sa ho ge postgresql+psycopg 
 
 #  Ab ham chahta han jo bhi hmari communication ho database ka sath wo encrypt ho kah jai 
 #  so we will add secure socket layer(ssl)
-engine = create_engine(connection_string, connect_args={"sslmode":"require "},pool_recycle=300)
+engine = create_engine(connection_string, connect_args={"sslmode":"require"},pool_recycle=300,pool_size=10, echo=True)
 
 #   Hamara jo engine ha wo connection pool banai ga 
 #  wo ak connection hmara database ka sath nahi kara ga 
@@ -52,22 +56,197 @@ engine = create_engine(connection_string, connect_args={"sslmode":"require "},po
 #  jasa waiter ai ga order lai ga kitchen me jai ga aur wapis order lana chala jai ga 
 #  yani connection standby ho ga 
 
-#  pool_recycle matlab kah 300 sec yani 5 min bad connection recycle ho jai 
+#  pool_recycle : matlab kah 300 sec yani 5 min bad connection recycle ho jai 
+
+# echo=True : matlab engine jitna bhi kam kar raha ho ga yeh mujha har step ka show kar dai ga kah yeh  yeh kam kia ha . Step wise sari cheezain terminal pa batai ga kah kya kya step perform hua han
 
 # *******************************************************************
 
+# ab jo model bnaya tha table ka isa create bhi karna ha 
+
+
+def create_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+
+# ham na ak engine create kia ha jo hmari application ke communication karwai ga db ka sath 
+# Is engine ka use karta hua hamara tables database me create ho jai ga 
+# *******************************************************************
+#  Giving data to understand concept 
+
+# todo1 : Todo = Todo(content="will go for swimming")  
+# todo2 : Todo = Todo(content="Bring water")
+
+
+# *******************************************************************
+
+#  Session (Ak app me engine ak ho ga session different create ho ga)
+#  session: separate session for each functionality/transaction
+#  we will import  Session from sqlmodel
+
+#  hamra har session engine ko use karta hua koi bhi functionality provide kara ga 
+
+# session = Session(engine)
+
+# *******************************************************************
+
+# create todos in database
+
+#  session overwrite sa bachna ka lia kahta ha kah jitna bhi kam mera sa karwana han wo mujha da do me ak hi bar kar do ga
+
+# yeh in memory ariable me store kar lata h adata 
+# abhi direct commit nahi kara ga 
+#  Like in git pehla ap add kar lata han commit bad me karta han
+
+# session.add(todo1)
+# session.add(todo2)
+# print(f"Before Commit {todo1}")
+#  Yahan todo ka andar value ha 
+# session.commit()
+
+# session.refresh(todo1)
+
+#  yeh hama database sa value la kar da ga jahan id  bhi assign ho chuki ha 
+
+#  Commit sa todo table me create ho jai ga   
+# print(f"After Commit {todo1}")
+#  Yahan todo ka andar koi value nahi ha
+# session.close()
+
+#  Todo ke class as a table model use ho rahi ha wo hama yahan sa pta chal raha ha kah is ke basis pa tables  create ho raha han 
+
+
+# *******************************************************************
+
+#  Har transaction ka lia hama session required ha  
+#  session ko  open karain ga aur close karain ga jab transacion complete ho jai ge 
+#  Behtar yeh ha kah ham apna ak function bna lain 
+
+#  We will use generator function here 
+
+def get_session():
+    with Session(engine) as session:
+        yield session 
+# *******************************************************************
+#  life span ka me function bnao ga is ka lia mujha context manager import karna para ga 
+# from contextlib import asynccontextmanager
+#  yeh context bnai ga app ka kah kis tarah sa chalna chahia 
+#  Isa as a decorater use karo ga 
+#  necha jo bhi function ho ga isa as a context create kar da ga 
+# ab is ko async nahi bhi bnaya yeh async hi ho ga 
+#  is me asi cheezain karain ga kah jab app start hoti ha to pehla yeh kam hona chahia 
+# e.g  1) tables create hona chahia  2)
+
+#  app ke start pa sab sa pehla yeh kam hona chahia 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # 1) tables create hona chahia
+     print("Creating Tables")
+     create_tables()
+     print("Tables Created")
+     yield
+# yield pa  a kah run jai ga phir wahan pa baqi function hamari app ka cuncurrently perform ho ga 
+
+
+     
 
 
 
 
-app : FastAPI = FastAPI()
+# *******************************************************************
+# Jab hamari app start hoti ha to wo khuch cheezo pa depend kar rahi hoti ha
+# - Hama is ka lia ak context bnana para ga 
+#  app ko title aur version bhi da sakta han 
+app : FastAPI = FastAPI(lifespan=lifespan, title="TaskTrack todo app", version="1.0.0" )
 
+
+
+
+
+# *******************************************************************
+
+# In FastAPI, using async allows your application to handle multiple requests concurrently without blocking the execution of other requests. This improves the overall responsiveness and performance of your web application.
+
+#  ab har method(get, del, post,put) ka lia session ka sara kam karna para ga 
+#  Har transaction ka lia hama session required ha  
+#  session ko  open karain ga aur close karain ga jab transacion complete ho jai ge 
+#  Behtar yeh ha kah ham apna ak function bna lain 
 @app.get("/")
 async def root():
     return {"message":"Welcome to TaskTrack todo app"}
 
-@app.get("/todo")
-async def read_todos():
-    return {"content":"dummy_todo"}
+@app.post("/todos", response_model=Todo)
+#  user sa ak todo ai ge aus ke type ho ge todo
+#  Ab jo task database sa ai ge ham na isa create karna ha for that we need a session  
+# ab yeh session get_session ka function me rakh lia 
+#  Yahan ham dakh sakta han kah todo ke class as a data model bhi use ho rahi ha 
+
+#  response_model=Todo : Is ka yeh matlab ha kah mujha jo response/todo return ho ge wo validate ho ge is datamodel sa 
+# Agar ham user ka bnata to alag sa aus ka bhi model bna lata 
+#  agar ham chahta han kah ham asa data model banai jis sa table na bana to ham  table=True nahi likhain ga 
+async def create_todo(todo:Todo , session:Annotated[Session,Depends(get_session )]):
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
+
+@app.get("/todos",response_model=list[Todo])
+async def get_all_todos(session:Annotated[Session,Depends(get_session)]):
+    #  hama koi command dani para ge database ko orm ka through wo command jai ge 
+    #  psycopg ausa sql ke query me convert kar kah database ko da ga  
+    #  database ausa aus command ke base pa value return kar kah da dai ga 
+    statement = select(Todo)
+    # Todos ka table select karain 
+    todos =  session.exec(statement).all()
+    #  return ka datatype btana para ga datavalidation ka lia 
+    #  ak sa zyada todos ai ge to wo list ke form me ai ge 
+    if todos:
+        return todos
+    else:
+        raise HTTPException(status_code=404 , detail=f"No record found")
+
+@app.get("/todos/{id}", response_model=Todo)
+async def get_single_todo(id:int, session:Annotated[Session,Depends(get_session)]):
+    todo =  session.exec(select(Todo).where(Todo.id == id)).first()
+    #  asal me yeh beshak single todo ha lakin yeh list ke form me ai ge 
+    #  ham chahta han single object/ dictionary   me ai to .first() use karna ho ga 
+    #  .first() =====>>>>>>>> Fetch the first object or None if no object is present.
+    if todo:
+        return todo
+    else:
+        raise HTTPException(status_code=404 , detail=f"No todo found with id: {id}")
+
+@app.put("/todos/{id}")
+#  put me complete data provide karna parta ha 
+# Patch me jo data change karna chahta han sirf wohi provide karna chahta han  
+async def update_todo(id:int, todo:Todo, session:Annotated[Session,Depends(get_session)]):
+    existing_todo = session.exec(select(Todo).where(Todo.id==id)).first()
+    if existing_todo:
+        existing_todo.content = todo.content
+        existing_todo.is_completed = todo.is_completed
+        session.add(existing_todo)
+        session.commit()
+        session.refresh(existing_todo)
+        return existing_todo
+    else:
+        raise HTTPException (status_code=404 , detail=f"No todo found with id: {id}")
+
+
+@app.delete("/todos/{id}")
+async def delete_todo(id:int, session:Annotated[Session,Depends(get_session)]):
+    todo = session.exec(select(Todo).where(Todo.id == id)).first()
+    # todo = session. get (Todo, id)
+    if todo:
+        session.delete(todo)
+        session.commit()
+        #  no need to refresh this time as we don't need it in return 
+        return {"message":"Task successfully deleted"}
+    else:
+        raise HTTPException (status_code=404 , detail=f"No todo found with id: {id}")
+    
+
+
+    
 
 
